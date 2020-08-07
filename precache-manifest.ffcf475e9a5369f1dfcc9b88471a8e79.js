6faeaf234681168adb83dc5f323ead5ec3fed3cc
Denis Rybalka <denis.rubalka1@gmail.com>
2020-08-07 15:47:09 +0300

    Updates

---
+++ precache-manifest.ffcf475e9a5369f1dfcc9b88471a8e79.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7677634a2f8be86d56d986a4227d107f",
+    "url": "/demo-repo/index.html"
+  },
+  {
+    "revision": "47a8c71a71913565deb1",
+    "url": "/demo-repo/static/css/main.7e68c950.chunk.css"
+  },
+  {
+    "revision": "614a2381a2f2456d13cc",
+    "url": "/demo-repo/static/js/2.7652a70a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/demo-repo/static/js/2.7652a70a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47a8c71a71913565deb1",
+    "url": "/demo-repo/static/js/main.263d3887.chunk.js"
+  },
+  {
+    "revision": "44ff5fb71ce22c002842",
+    "url": "/demo-repo/static/js/runtime-main.9d318ead.js"
+  },
+  {
+    "revision": "9d764eba6548244d8343bfc9d80e26b6",
+    "url": "/demo-repo/static/media/Polygon 2.9d764eba.svg"
+  },
+  {
+    "revision": "51f4af352b0dde0dd4cf96abb0a8f77f",
+    "url": "/demo-repo/static/media/close.51f4af35.svg"
+  }
+]);
